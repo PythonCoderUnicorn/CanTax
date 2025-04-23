c8acd5d0336249fc15455fc254886882bb127f72
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2025-04-23 06:45:32 -0600

    Add files via upload

---
+++ chap1.py
@@ -0,0 +1,70 @@
+
+"""
+==================================
+     CANADIAN INCOME TAXATION
+   planning and decision making
+        2022-2023 edition
+ Buckwold | Kitunen | Roman | Iqbal
+        McGraw Hill
+=================================
+"""
+
+"""
+# Taxation and financial decision process
+
+1. tax should be considered a controllable cost
+
+    business in 1 province has 3 options for selling in another province:
+    a) have direct sales with sales agents who travel to province
+    b) set up a new small scale branch for sales inside province
+    c) set up a separate corporation inside province 
+
+2. cash flow exists only on an after-tax basis
+
+cash flow = revenue, expense, asset  aquisition, divesture , debt or equity restructuring
+'after tax' decision making is business decision making
+
+
+# Fundamental Income Tax Structure & its complexity
+
+taxpayers                           = three entities subject to tax in Canada
+types of tax                        = each taxpayer can earn 5 types of income, 4 of them have general application to business activity
+business & investment structures    = 6 basic forms of organization in which a business or investing activity can take place. 3 are directly subject to tax
+tax jurisdiction                    = Canadian busines or investor is normally subject to 2 taxation authorities unless it extends its operations to international markets
+
+
+"""
+
+primary_types_of_income = ['Business','Property','Employment','Capital Gains'] 
+
+entities_subject_to_taxation_income = ['Individuals','Corporations','Trusts']
+
+
+# alternative forms of business & investing structures used by taxable entities
+alt_structures_taxable_entities = [
+    'Proprietorship',
+    'Corporation',
+    'Partnership',
+    'Limited partnership',
+    'Joint venture',
+    'Income trust'
+]
+
+tax_jurisdictions = [
+    'Provincial',
+    'Territorial',
+    'Federal',
+    'Foreign'
+]
+
+"""
+Example:
+
+2 investor {corporations} (taxpayers) jointly enter into a new venture that
+will earn business {income} (type of income) could be use a {partnership},
+a {joint venture} or a {separate corporation} (business structure). each type of
+structure may affect the amount and timing on operating results
+
+"""
+
+
