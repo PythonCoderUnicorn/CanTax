de7d8c6b1c50327b5e3136a0f1165bb88d8dcf99
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2025-04-24 14:25:47 -0600

    Add files via upload

---
+++ chap3.py
@@ -0,0 +1,476 @@
+
+"""
+==================================
+     CANADIAN INCOME TAXATION
+   planning and decision making
+        2022-2023 edition
+ Buckwold | Kitunen | Roman | Iqbal
+        McGraw Hill
+=================================
+"""
+
+
+primary_types_of_income = ['Business','Property','Employment','Capital Gains'] 
+
+entities_subject_to_taxation_income = ['Individuals','Corporations','Trusts']
+
+
+# alternative forms of business & investing structures used by taxable entities
+alt_structures_taxable_entities = [
+    'Proprietorship',
+    'Corporation',
+    'Partnership',
+    'Limited partnership',
+    'Joint venture',
+    'Income trust'
+]
+
+tax_jurisdictions = [
+    'Provincial',
+    'Territorial',
+    'Federal',
+    'Foreign'
+]
+
+
+
+
+"""
+## Sources of Canadian Tax Law
+
+2 part structure:
+
+1. parties subject to tax 
+2. how to determine the income to which the tax is applied
+
+## Sources of Canadian Tax Law
+
+3 separate sources govern income tax law in Canada:
+
+- a) statute law = Income Tax Act (all but Quebec adopted)
+- b) common law
+- c) international tax conventions
+
+
+## Liability for Tax
+
+- a) the entities = individuals, corporations, trusts
+- b) resident individuals and corporations
+
+
+### individuals
+
+concept of 'residency' is key to determining an individual's obligation to pay tax
+
+factors:
+
+- amount of time spent in Canada on regular basis
+- motives for being present or absent
+- maintenance of a dwelling place in Canada
+- origin and background of the individual
+- general mode & routine of the individual's life
+- existence of social and financial connections with Canada
+
+
+Corporations
+
+all corporations incorporated in Canada = residents, subject to tax
+foreign corporations can be considered residents if 'central management and control' resides in Canada
+
+dual jurisdictions
+
+Non-residents (individual & corporations)
+
+- not subject to tax but some income based on activities can be
+
+- international business transactions broad categories:
+    1. transactions that are started and care completed in foreign host country
+    2. transactions that originate in one country but concluded in second country
+    
+1. tax on net income (revenues - expenses) for non-residents:
+
+    - carries on business in Canada
+    - disposes of certain Canadian property
+    - is employed in Canada
+
+2. tax on Canadian source income
+
+    - withholding tax = tax on amount that originates in Canada but is paid directly to non-resident who has to pay directly to tax authorities
+    - withholding tax rate = 25% by Income Tax Act
+    - types of payment may be subject to Canadian withholding tax when paid to non-residents:
+        - dividends
+        - rents
+        - royalties
+        - pension benefits
+        - registered retirement savings plan (RRSP) payments
+        - registered retirement income fund (RRIF) payments
+        - certain management and admin fees
+        - interest (only if paid to a non-arm's legth related party) 
+
+
+### basic Structures for foreign expansions
+
+```
+Structure A
+    individual --> Canada [corporation] --> foreign country: direct sales
+
+Structure B
+    individual --> Canada [corporation] --> foreign country: foreign branch
+
+Structure C
+    individual --> Canada [corporation] --> foreign country: foreign corporation
+```
+
+
+
+## Determination of Income
+
+*FUN FACT* Income Tax Act does not specifically provide a single definition of income
+
+income:
+
+1. each entity subject to tax determines its taxable income on basis of a taxation year
+2. income for each entity includes and is restricted to the world income generated from 5 categories, each category is determined by set of fundamental principles
+3. net incomes for each of the 5 categories of income is reduced by a limited number of specific items
+
+taxable income for a taxation year of an entity subject to tax in Canada formula
+
+- `T1 = net_income - Division_C_deductions`
+
+- Taxation Year - Corporations : the fiscal period (any time period not exceeding 53 weeks) for which an entity accounts for its financial affairs
+    
+- Taxation Year - Individuals: simply the calendar year, 12 month period ending on December 31 every year
+
+
+
+## Types of Income
+
+- employment income
+- business income
+- property income
+- capital gains & losses 
+- other specific sources 
+
+
+
+
+## Administration of the income tax system
+
+- who must file tax returns and when they must be filled out
+- procedures for assessing returns that have been filed
+- deadlines for income tax payments
+- taxpayer's rights and procedures for appealing disputed assessments
+
+
+
+## Filing Returns
+
+every taxpayer **must** file an annual income tax return 
+
+1. Corporations
+    every corporation must file an annual income tax return within 6 months of its taxation year-end (even if it has no income or has no financial activity)
+
+| tax-year end | filing deadline |
+|--------------|-----------------|
+| March 31     | September 30    |
+| April 30     | October 30      |
+| August 31    | February 28     |
+| September 10 | March 10        |
+
+2. Individuals -
+    every taxpaper who is an individual must file a tax return by April 30 for the most recent calendar year.
+    June 15 date is if the individual or spouse carries on a business.
+
+3. Trusts & Estates -
+    every trust must file an income tax return with 90 days of its taxation year.
+
+4. Tax returns filed electronically -
+    the Canada Revenue Agency (CRA) does not consider a filed return until a receipt of tax return is given.
+
+| Entity      | latest filing date          |
+|-------------|-----------------------------|
+| corporation | 6 month after taxation year |
+| individual  | April 30 or June 15 (business)       |
+| deceased individual  | April 30 or June 15 (business) or 6 months from date of death      |
+| trusts  | 90 days after taxation year     |
+
+
+## Payment of Tax
+
+
+
+### Individuals
+
+Individuals must pay tax on certain types of income at the time of 
+income is received with other types of income, payments must be made on an installment basis.
+
+- employment income, the employer is required to withhold the appropriate amount of tax and remit it to the CRA
+- tax must also be withheld for retiring allowances, employment insurance (EI) benefits, pension benefits and
+payments to RRSPs
+
+INSTALLMENTS
+
+income that is not subject to withholding taxes at source may be subject to quarterly tax installments
+
+- tax owing (federally and provincially after deducting taxes withheld at source) exceed $3,000 for both 
+current year and either of the past 2 taxation years
+
+- a) 1/4 of the estimated tax payable that is not withheld at source
+- b) 1/4 of the previous year's tax liability that was not withheld at source
+- c) March & June installments: 1/4 of 2nd preceding year's tax liability that was not withheld at source
+- September & December installments: 1/2 of the preceding year's tax liability that was not withheld at source in excess 
+of the March & June installment payments
+
+it is up to the taxpaper to estimate the current year's tax if they want to pay a lower installment
+
+- for federal and provincial taxes < $3,000 there is no installments need to be made 
+
+- balance due April 30th of the following year
+
+
+
+
+## Payroll Taxes 
+
+Employers are required to deduct employment insurance (EI) premiums, Canada Pension
+Plan (CPP) contributions and income tax from remuneration and other
+types of income paid to employees.
+
+- required EI premiums are `1.58%` of insurable earnings up to yearly 
+maximums (year 2022: max was $60,300 for insurable earnings and 952.74 (0.0158 x 60300) for EI )
+
+- employer must contribute 1.4 times the amount of the EI premiums deducted from employee's renumeration
+
+- CPP rate is `5.7%` in 2022. the required employee CPP contributions are 5.7% of
+(pensionable earnings - basic exemption $3500)
+
+- the maximum annual pensionable earnings for 2022 are $64,900.
+- max annual employee CPP contributions (64900 - 3500 x 0.057) is $3,499.80
+- an employer must contribute an amount equal to the CPP contributions deducted from employee's remuneration
+
+Employee CPP contributions have 2 parts
+
+- base: CPP contribution (max #3039.30 for 2022) calculated at `4.95%`
+- CPP enhacned contribution (max $460.50 for 2022)
+- the difference (5.7 - 4.95 = 0.75) are enhanced contributions and are deductible by the employee in computing net income for tax purposes
+
+
+Avg Monthly Remittance   
+
+- `if` average monthly remittance <= $3,000 in 1st or 2nd calendar year 
+`then` 15th of the month at end of each quarter
+(April 15, July 15, October 15 and January 15)
+
+- `if` average monthly remittance <= $25,000 `then` 15th of the month following the month which the payroll deductions relate
+
+- `if` average monthly remittance >= 25,000 and <= 100,000 `then` 
+    
+    a)  remuneration paid in first 15 days of the month by the 25th day of the same month
+        
+    b) remuneration paid from the 16th to the end of the month but before the first day of the 
+    following month, by the 10th day of the following month
+
+- `if` average monthly remittance >= 100,000 `then` 3 working days following the 7th, 14th, 21st and last day of the month
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+import streamlit as st 
+import pandas as pd 
+
+st.divider()
+
+st.title("Income Tax ")
+
+st.write("sections for determining Net Income for Tax Purposes *Section 3 Income Tax Act*")
+
+# statutory scheme: formula for determining Net Income for Tax Purposes [Section 3 ITA]
+
+income_df1 = pd.DataFrame(
+    [
+        {"paragraph 3(a)": "Employment Income: Salary", "amount": 0},
+        {"paragraph 3(a)": "Business Income", "amount": 0 },
+        {"paragraph 3(a)": "Property Income: Interest", "amount": 0 },
+        {"paragraph 3(a)": "Property Income: Net rental income", "amount": 0 },
+        {"paragraph 3(a)": "Other Income: RRSP", "amount": 0 },
+    ]
+)
+
+
+income_df2 = pd.DataFrame(
+    [
+        {"paragraph 3(b)": "Taxable capital gains: public corporation shares", "amount": 0 } ,
+        {"paragraph 3(b)": "Taxable net gain from listed personal property", "amount": 0 } ,
+        {"paragraph 3(b)": "Allowable capital losses: bonds", "amount": 0 } ,
+
+    ]
+)
+
+
+
+
+income_df3 = pd.DataFrame(
+    [
+        {"paragraph 3(c)": "other deductions", "amount": 0}
+    ]
+)
+
+
+
+income_df4 = pd.DataFrame(
+    [
+        {"paragraph 3(d)": "Employment losses", "amount": 0},
+        {"paragraph 3(d)": "Business losses: partnership", "amount": 0},
+        {"paragraph 3(d)": "Property losses", "amount": 0},
+        {"paragraph 3(d)": "Allowable business investment losses", "amount": 0},
+    ]
+)
+
+edited_df1 = st.data_editor(income_df1, num_rows="fixed")
+edited_df2 = st.data_editor(income_df2, num_rows="fixed")
+edited_df3 = st.data_editor(income_df3, num_rows="fixed")
+edited_df4 = st.data_editor(income_df4, num_rows="fixed")
+
+
+
+
+
+
+
+
+
+
+
+st.divider()
+
+
+
+
+st.write(f"### Estimated tax payable installments - Individuals")
+
+col10, col11, col13 = st.columns(3)
+with col10:
+    total_tax_yr_1 = st.number_input("Total tax liability for year 1")
+
+with col11:
+    total_tax_yr_2 = st.number_input("Total Tax liability for year 2")
+
+with col13:
+    estimated_total_tax_yr_3 = st.number_input("Estimated Tax liability for year 3")
+
+
+col30,col31,col32,col33 = st.columns(4)
+
+payment_installments = ['March 15','June 15','September 15','December 15']
+payment_months = (total_tax_yr_1/ len(payment_installments))
+
+with col30:
+    amount_payable_1 = (1/4 * estimated_total_tax_yr_3)
+    st.metric("a) tax payable (Yr 3)",value= f"${amount_payable_1}")
+
+with col31:
+    amount_payable_2 = (1/4 * total_tax_yr_2)
+    st.metric("b) tax payable (Yr 2)",value= f"${amount_payable_2}")
+
+with col32:
+    amount_payable_3 = (1/4 * total_tax_yr_1) 
+    st.metric("c) tax payable (March & June)",value= f"${amount_payable_3}")
+
+with col33:
+    amount_payable_4 = (total_tax_yr_2 - (payment_months*2)) * 1/2
+    st.metric("c) tax payable (Sept & Dec)",value= f"${amount_payable_4}")
+
+
+
+
+st.write()
+
+
+
+
+st.write(f"### Estimated tax payable installments - Corporations")
+
+col40, col41, col42 = st.columns(3)
+with col40:
+    corp_total_tax_yr_1 = st.number_input("Corporate tax liability (year 1)")
+
+with col41:
+    corp_total_tax_yr_2 = st.number_input("Corporate tax liability (year 2)")
+
+with col42:
+    corp_estimated_total_tax_yr_3 = st.number_input("Corporate tax liability (year 3)")
+
+
+
+install_a = (1/12 * corp_estimated_total_tax_yr_3)
+install_b = (1/12 * corp_total_tax_yr_2)
+
+
+st.write(f"Payment installment option : a)  ${install_a:,.2f}  ")
+st.write(f"Payment installment: option: b)  ${install_b:,.2f} ")
+
+
+
+
+
+
+st.divider()
+
+st.write("## Calculate Net Income")
+
+"""
+Calculate the net income for tax purposes
+"""
+
+
+
+
+
+cola,colb,cold = st.columns([1,1,1], vertical_alignment="top")
+
+with cola:
+    st.header("section 3(a)")
+    section_3a_1 = st.number_input("Employment Income: Salary", min_value=0)
+    section_3a_2 = st.number_input("Business Income", min_value=0)
+    section_3a_3 = st.number_input("Property Income: Interest", min_value=0)
+    section_3a_4 = st.number_input("Property Income: Net Income", min_value=0)
+    section_3a_5 = st.number_input("Other Income: RRSPs etc.", min_value=0)
+ 
+    total_section3a = section_3a_1+section_3a_2+section_3a_3+section_3a_4+section_3a_5 
+    st.write(total_section3a)
+
+with colb:
+    st.header("section 3(b)")
+    section_3b_1 = st.number_input("Taxable Capital Gains: Corp. shares", min_value=0)
+    section_3b_2 = st.number_input("Allowable Capital loss: sale of bonds", min_value=0)
+
+    total_section3b =  section_3b_1 + section_3b_2
+    # 3(c) other deductions
+    total_section3a - total_section3b
+
+
+with cold:
+    st.header("section 3(d)")
+    section_3d_1 = st.number_input("Business loss: partnership", min_value=0)
+    section_3d_2 = st.number_input("Allowable business investment loss: shares", min_value=0)
+
+    total_section3d =section_3d_1 + section_3d_2
+    st.write(total_section3d)
+
+
+    net_income_for_tax = total_section3a - (total_section3b) - total_section3d
+    st.metric("Net income", value=f"${net_income_for_tax:,.2f}")
+
+
+
